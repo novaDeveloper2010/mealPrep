a9df2cf55f7a249dfcbd016f424ba2f356e5cadd
Renzo <17202205+renzonovadeveloper@users.noreply.github.com>
2025-11-22 12:54:22 -0500

    adding more plates

---
+++ password-config.js
@@ -0,0 +1,14 @@
+/**
+ * Password Protection Configuration
+ *
+ * To change the password, edit the value below.
+ * Note: This is basic protection, not secure for sensitive data.
+ */
+
+const PASSWORD_CONFIG = {
+    // Set your password here
+    password: "mealprep123",
+
+    // Set to false to disable password protection
+    enabled: false
+};
